010aefc507e8abada07984ab04eae3d304a5e792
aaveggupta <aaveggupta.contact@gmail.com>
2023-01-22 08:45:57 +0530

    feat: implement header and footer component

--- components/utility/search/Search.tsx
+++ components/utility/search/Search.tsx
@@ -1,10 +1,8 @@
 import { useState } from 'react';
 
-export interface ISearch {
-  sampleTextProp: string;
-}
+export interface ISearch {}
 
-const Search: React.FC<ISearch> = ({ sampleTextProp }) => {
+const Search: React.FC<ISearch> = () => {
   const [searchTerm, setSearchTerm] = useState<string>();
 
   const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
